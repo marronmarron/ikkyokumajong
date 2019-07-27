b4874bdc6241c1435fa3db54c25921c25543c3d6
Itsuki Kanemoto <kanemoto@vcube.co.jp>
2019-07-28 02:10:26 +0900

    ４人ごとに部屋を分ける

--- main.js
+++ main.js
@@ -2,22 +2,49 @@ const _ = require('lodash');
 
 module.exports = io => {
     // サーバー側処理のエントリポイント
-    let yama = createYama();
+    let current_room_id = 0;
 
-    io.on('connection', socket => {
-        socket.emit('haipai', haipai(yama));
+    io.on('connection', async socket => {
+        const room_id = current_room_id;
+        const room_name = 'room_'+room_id;
 
-        socket.on('dahai', function(dahai) {
-            console.log(dahai);
-            socket.emit('tsumo', yama.pop());
-        });
+        console.log(socket.id);
 
-        socket.on('restart', () => {
-            yama = createYama();
-            socket.emit('haipai', haipai(yama));
-        });
+        socket.join(room_name);
+        const clients = await getClientsPromise(io.in(room_name));
+        if(clients.length === 4) {
+             gameStart(io, room_name, clients);
+            current_room_id++;
+        }
     });
 };
+
+function gameStart(io, name, clients) {
+    console.log("GAME START "+name);
+    let yama = createYama();
+    _.forEach(clients, (id => io.to(id).emit('haipai', haipai(yama))));
+
+    let turn = 0;
+    io.to(name).emit('tsumo', yama.pop());
+
+    io.to(clients[turn]).on('dahai', function(dahai) {
+        console.log(dahai);
+        socket.emit('tsumo', yama.pop());
+    });
+
+    io.on('restart', () => {
+        gameStart(io, name, clients);
+    });
+}
+
+async function getClientsPromise(namespace) {
+    return new Promise((resolve, reject) => {
+        namespace.clients((err, clients) => {
+            if (err) reject(err);
+            else resolve(clients);
+        });
+    });
+}
 
 function haipai(yama) {
     const tehai = [];
--- public/javascripts/room.js
+++ public/javascripts/room.js
@@ -1,7 +1,5 @@
 const socket = io();
 
-// socket.emit('dahai', 3);
-
 pai_img = [];
 for (let i = 0; i < 34; i++) {
     var img = new Image();
@@ -9,26 +7,22 @@ for (let i = 0; i < 34; i++) {
     pai_img[i] = img;
 }
 
-document.getElementById('canvas').style.visibility = "hidden";
-
 socket.on('haipai', haipai => {
-    console.log(haipai);
+    console.log('haipai : ' + haipai);
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
     let left = 0;
     const pai_wid = pai_img[0].width;
     for (let i = 0; i < 13; i++) {
-        console.log(left);
         ctx.drawImage(pai_img[Math.floor(haipai[i] / 4)], left, 10);
         left += pai_wid;
     }
 });
 
 socket.on('tsumo', function (tsumoPai) {
-    console.log(tsumoPai);
+    console.log('tsumo : ' + tsumoPai);
 });
 
 document.getElementById('restart-button').addEventListener("click", ()=>{
-    document.getElementById('canvas').style.visibility = "visible";
     socket.emit('restart');
 });
