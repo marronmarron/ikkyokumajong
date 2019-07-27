9147ac6c904573837eed132639329f671c31675b
Itsuki Kanemoto <kanemoto@vcube.co.jp>
2019-07-27 18:35:29 +0900

    ランダムな配牌とツモができる

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-module.exports = function(io) {
+const _ = require('lodash');
+
+module.exports = io => {
     // サーバー側処理のエントリポイント
-    io.on('connection', function(socket) {
+    const yama = _.shuffle([...Array(136).keys()]);
+
+    io.on('connection', socket => {
         console.log("user connected");
+        console.log(haipai(yama));
+        socket.emit('haipai', haipai(yama));
         socket.on('dahai', function(dahai) {
             console.log(dahai);
-            socket.emit('tsumo', 1);
+            socket.emit('tsumo', yama.pop());
         });
     });
 };
+
+function haipai(yama) {
+    const tehai = [];
+    _.times(13, () => {tehai.push(yama.pop())});
+    return tehai;
+}
--- public/javascripts/room.js
+++ public/javascripts/room.js
@@ -2,6 +2,12 @@ const socket = io();
 
 socket.emit('dahai', 3);
 
+
+
+socket.on('haipai', haipai => {
+    console.log(haipai);
+});
+
 socket.on('tsumo', function(tsumoPai) {
     console.log(tsumoPai);
 });
