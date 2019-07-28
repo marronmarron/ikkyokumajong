e6a941c26d621fcaf738b6d6b69f214ee68a552c
Kido <sample@email.com>
2019-07-28 13:29:46 +0900

    打牌ができるようにした

--- main.js
+++ main.js
@@ -1,5 +1,75 @@
 const _ = require('lodash');
 
+<<<<<<< Updated upstream
+=======
+class GameState {
+    io;
+    room;
+    player;
+    turn = 0;
+    yama;
+
+    constructor(io, room) {
+        this.io = io;
+        this.room = room;
+        this.player = [];
+        this.yama = createYama();
+    }
+
+    addPlayer(socket) {
+        socket.join(this.room);
+        this.player.push(socket.id);
+        if(this.player.length === 1) {
+            this.gameStart();
+            currentGameState = new GameState(this.io, 'room_' + ++current_room_id);
+        }
+
+        socket.on('dahai', pai => {
+            if(socket.id !== this.player[this.turn]) {
+                return;
+            }
+            console.log('dahai : player=' + this.turn + 'pai=' + pai);
+            this.turn = ++this.turn%4;
+            this.tsumo(this.turn);
+        });
+
+        socket.on('restart', () => {
+            if(this.player.length === 1) {
+                this.gameStart();
+            }
+        });
+    }
+
+    removePlayer(id) {
+        this.player = this.player.filter(p => p !== id);
+    }
+
+    tsumo(turn) {
+        const pai = this.yama.pop();
+        console.log('tsumo : player=' + turn + 'pai=' + pai);
+        this.io.to(this.player[turn]).emit('tsumo', pai);
+    }
+
+    gameStart() {
+        console.log("GAME START " + this.room);
+        this.yama = createYama();
+        _.forEach(this.player, (id => this.io.to(id).emit('haipai', this.haipai())));
+
+        console.log("tsumo : " + this.player[this.turn]);
+        this.io.to(this.player[this.turn]).emit('tsumo', this.yama.pop());
+    }
+
+    haipai() {
+        const tehai = [];
+        _.times(13, () => {tehai.push(this.yama.pop())});
+        return tehai;
+    }
+}
+
+let currentGameState;
+let current_room_id = 0;
+
+>>>>>>> Stashed changes
 module.exports = io => {
     // サーバー側処理のエントリポイント
     const yama = _.shuffle([...Array(136).keys()]);
--- public/javascripts/room.js
+++ public/javascripts/room.js
@@ -9,9 +9,21 @@ for (let i = 0; i < 34; i++) {
     pai_img[i] = img;
 }
 
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+let tehai = []
+pai_img[0].addEventListener('load', function() {
+    pai_wid = pai_img[0].width;
+    pai_hei = pai_img[0].height;
+    left = (canvas.width - 13 * pai_wid) / 2;
+    tsumo_left = (canvas.width + 13 * pai_wid) / 2 + 8;
+})
+
+
 socket.on('haipai', haipai => {
     console.log(haipai);
     haipai.sort((a, b) => a - b);
+<<<<<<< Updated upstream
     console.log(haipai);
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
@@ -28,3 +40,56 @@ socket.on('haipai', haipai => {
 socket.on('tsumo', function (tsumoPai) {
     console.log(tsumoPai);
 });
+=======
+    tehai = haipai;
+    drawTehai();
+});
+
+socket.on('tsumo', function (tsumoPai) {
+    console.log('tsumo : ' + tsumoPai);
+    ctx.drawImage(pai_img[Math.floor(tsumoPai / 4)], tsumo_left, canvas.height - pai_hei);
+    tehai.push(tsumoPai)//一時的に手牌１４枚になる
+});
+
+function dahai(te_num) {
+    const da_pai = tehai[te_num];
+    console.log('dahai : ' + da_pai);
+    socket.emit('dahai', da_pai);
+    tehai[te_num] = tehai[13];
+    tehai.pop();
+    tehai.sort((a, b) => a - b);
+    drawTehai();
+}
+
+document.getElementById('restart-button').addEventListener("click", ()=>{
+    console.log("restart");
+    socket.emit('restart');
+});
+
+function onClick(e) {
+    if (tehai.length < 14) return;
+    const x = e.clientX - canvas.offsetLeft;
+    const y = e.clientY - canvas.offsetTop;
+    if (x < left || y > canvas.height || y < canvas.height - pai_hei) return;
+    for (let i=0; i<13; ++i) {
+        if (x < left + (i+1) * pai_wid) {
+            dahai(i);
+            return;
+        }
+    }
+    if (x >= tsumo_left && x < tsumo_left + pai_wid) {
+        dahai(13);
+    }
+}
+
+function drawTehai() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    let le = left;
+    for (let i = 0; i < 13; i++) {
+        ctx.drawImage(pai_img[Math.floor(tehai[i] / 4)], le, canvas.height - pai_hei);
+        le += pai_wid;
+    }
+}
+
+canvas.addEventListener('click', onClick, false);
+>>>>>>> Stashed changes
